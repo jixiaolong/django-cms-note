9c0e98087832e09a47cfef906109a739d78d3076
jixiaolong <jixiaolongcom@sina.cn>
2013-10-15 12:10:02 +0800

    添加导航

---
+++ views/basic.py
@@ -0,0 +1,6 @@
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    return render_to_response("home.html",context_instance=RequestContext(request))
